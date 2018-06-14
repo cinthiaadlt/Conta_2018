475eaf50ae5133eca93d30c1e339a31610a18a09
Cinthia <cinthia.alvarezs3@gmail.com>
2018-06-14 16:58:00 -0400

    chau comentarios
    chau comentarios de otros autores

--- php/agregar-cuentas.php
+++ php/agregar-cuentas.php
@@ -1,24 +1,3 @@
-<?php
-/*~ Archivo agregar-cuentas.php
-.---------------------------------------------------------------------------.
-|    Software: CAS - Computerized Accountancy System                        |
-|     Versión: 1.0                                                          |
-|   Lenguajes: PHP, HTML, CSS3 y Javascript                                 |
-| ------------------------------------------------------------------------- |
-|   Autores: Ricardo Vigil (alexcontreras@outlook.com)                      |
-|          : Vanessa Campos                                                 |
-|          : Ingrid Aguilar                                                 |
-|          : Jhosseline Rodriguez                                           |
-| Copyright (C) 2013, FIA-UES. Todos los derechos reservados.               |
-| ------------------------------------------------------------------------- |
-|                                                                           |
-| Este archivo es parte del sistema de contabilidad C.A.S para la cátedra   |
-| de Sistemas Contables de la Facultad de Ingeniería y Arquitectura de la   |
-| Universidad de El Salvador.                                               |
-|                                                                           |
-'---------------------------------------------------------------------------'
-*/
-?>
 <?php 
 
 /* Evita que aparezcan notificaciones en la página. */
