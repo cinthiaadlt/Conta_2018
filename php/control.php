b443920ed74b2ee6e3c8b48333ad9a96411f561d
Cinthia <cinthia.alvarezs3@gmail.com>
2018-06-14 23:51:57 -0400

    modificacion del  sha1
    ahora sirve la encriptacion de contrseñas

--- php/control.php
+++ php/control.php
@@ -3,7 +3,7 @@
 
 	$usuario = $_POST["user_txt"];
 	$password = $_POST["password_txt"];
-	$consulta = "SELECT usuario FROM usuario WHERE usuario='$usuario' AND password='$password'";
+	$consulta = "SELECT usuario FROM usuario WHERE usuario='$usuario' AND password=sha1('$password')";
 	
 	$ejecutar_consulta = $conexion->query($consulta);
 
